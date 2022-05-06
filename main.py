b96684cb7cf85f3393a84d7af84b7ac75b20df64
Bryan Chia <bryancws@stanford.edu>
2022-05-06 10:32:11 -0700

    metrics calculations changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from lightning import (get_task,
                        get_logger)
 
 
-def train(dataset_folder="./data_files_test",
+def train(dataset_folder="./data_files",
           save_dir="./sandbox",
           exp_name="DemoExperiment",
           model="ResNet18",
